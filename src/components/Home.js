717bda2a4c317a1f210ba6115d089a33e05af989
Vbeni <benivillafana@gmail.com>
2023-08-03 18:15:01 -0700

    passed home component to app.js

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='container mt-5'>
+        <div className='row justify-content-center'>
+            <div className='col-lg-8'>
+                <h1 className='display-4'>West Coast Tech Job Market Analysis</h1>
+                <p className='lead'>An analysis of software engineering job distribution, tech trends, and salaries on the West Coast.</p>
+            </div>
+            </div>
+    </div>
+  )
+}
+
+export default Home
